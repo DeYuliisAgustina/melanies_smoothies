25af343fe7ab681ea9724e91bac38e0abbdf2f53
Agustina De Yuliis <138722535+DeYuliisAgustina@users.noreply.github.com>
2025-06-18 17:09:44 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,7 @@ if ingredients_list:
     import requests
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
     st.text(smoothiefroot_response)
+    
     #Si todo se ve bien, podés comentar st.stop() y descomentar la inserción:
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
